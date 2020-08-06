59eee82fa0c7ba3d71dcf326b35007f9d4a63b14
JC <jcm.tbest@gmail.com>
2020-08-06 11:58:01 -0500

    Modificado el archivo readme con la descripción del script de python

--- cCodeGenerator/cCodeGenerator.py
+++ cCodeGenerator/cCodeGenerator.py
@@ -1,18 +1,6 @@
 """
 Generador de código para identificar si un número es par o impar en lenguaje C
 by JChaconMencez
-
-La estructura original es:
-function esPar(numero) {
-if(numero == 1) {
-return false
-}
-}else if(numero == 2) {
-return true
-}
-}else if(numero == 3) {
-return false;
-
 """
 
 import numpy as np
@@ -31,42 +19,48 @@ def writetextfile(namefile, text):  # Función para escribir un string en un fic
 
 # Inicio del script
 nameFile = "par.c"
-num2writeCcode = 9
+num2writeCcode = 10
 try:
-    os.remove(nameFile)
-    print("Previous file removed!")
-except:
-    pass
+    num2writeCcode = int(input("Ingrese hasta que numero desea que pueda comparar el script: "))
+    try:
+        os.remove(nameFile)
+        print("Previous file removed!")
+    except:
+        pass
+    text2write = "#include <stdio.h>"  # Inclusion de la biblioteca stdio.h para entradas y salidas en C
+    text2write += '\n\nint esPar(int numero) {' \
+                  '\n\tif(numero == 1) {' \
+                  '\n\t\treturn 0;'
 
+    for i in range(num2writeCcode - 1):
+        if (i + 2) % 2:
+            text2write += '\n\t} else if(numero == ' + str(i + 2) + ') {' \
+                                                                    '\n\t\treturn 0;'
+        else:
+            text2write += '\n\t} else if(numero == ' + str(i + 2) + ') {' \
+                                                                    '\n\t\treturn 1;'
 
+    text2write += '\n\t}\n}'
+    # Inclusion de una funcion main para que se pueda compilar de manera autonoma por medio de gcc
+    text2write += '\nint main() {' \
+                  '\n\tint i;' \
+                  '\n\tprintf("Ingresa un número: \\n");' \
+                  '\n\tscanf("%d", &i);' \
+                  '\n\tprintf("\\nIngresaste: %d\\n", i);' \
+                  '\n\tint par = esPar(i);' \
+                  '\n\tif (par==1){' \
+                  '\n\t\tprintf("El número es par!\\n");' \
+                  '\n\t} else {' \
+                  '\n\t\tprintf("El número no es par!\\n");' \
+                  '\n\t}\n\treturn 0;' \
+                  '\n}'
 
-text2write = "#include <stdio.h>"                   # Inclusion de la biblioteca stdio.h para entradas y salidas en C
-text2write += '\n\nint esPar(int numero) {' \
-              '\n\tif(numero == 1) {' \
-              '\n\t\treturn 0;'
-
-for i in range(num2writeCcode-1):
-    if (i+2)%2:
-        text2write += '\n\t} else if(numero == ' + str(i+2) +') {' \
-                      '\n\t\treturn 0;'
+    fileWrited = writetextfile(nameFile, text2write)
+    if fileWrited:
+        print("\nCodigo C creado con exito!")
     else:
-        text2write += '\n\t} else if(numero == ' + str(i+2) + ') {' \
-                      '\n\t\treturn 1;'
-
-text2write += '\n\t}\n}'
-# Inclusion de una funcion main para que se pueda compilar de manera autonoma por medio de gcc
-text2write += '\nint main() {' \
-              '\n\tint i;' \
-              '\n\tprintf("Ingresa un número: \\n");' \
-              '\n\tscanf("%d", &i);' \
-              '\n\tprintf("\\nIngresaste: %d\\n", i);' \
-              '\n\tint par = esPar(i);' \
-              '\n\tif (par==1){' \
-              '\n\t\tprintf("El número es par!\\n");' \
-              '\n\t} else {' \
-              '\n\t\tprintf("El número no es par!\\n");' \
-              '\n\t}\n\treturn 0;' \
-              '\n}'
+        print("\nError creando codigo C!")
+except:
+    print("Dato invalido!")
 
 
-writetextfile(nameFile, text2write)
